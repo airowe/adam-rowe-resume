1e24b70bcb4ba639827a56c072f0625d781ec654
Adam Rowe <adaminsley@gmail.com>
2020-04-01 15:26:55 -0400

    Work History, Github link

--- src/App.js
+++ src/App.js
@@ -2,21 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function MyInfo() {
+  return <div>
+            <h1>Adam Rowe</h1>
+            <p>I'm a mobile software engineer looking for work.</p>
+            <ul title="Work Experience">
+              <li>Le Tote - Lead iOS Engineer</li>
+              <li>Sysomos - Lead Android Engineer</li>
+              <li>Phoenix Software International - Java/Android Application Developer</li>
+            </ul>
+          </div>
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <MyInfo />
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/airowe"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github
         </a>
       </header>
     </div>
